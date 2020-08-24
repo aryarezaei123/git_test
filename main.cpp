63fd7b296cf717ad54f8ba10cbb76cda719b814d
Arya <rezaeiarya1997@gmail.com>
2020-08-24 14:53:39 +0430

    add subtract function

--- main.cpp
+++ main.cpp
@@ -2,15 +2,22 @@
 using namespace std;
 
 double add(double a, double b);
+double subtract(double a, double b);
 
 
 int main() {
     cout << add(12,4) << endl;
+    cout << subtract(12,4) << endl;
     return 0;
 }
 
 
 double add(double a, double b){
     double c = a + b;
+    return c;
+}
+
+double subtract(double a, double b){
+    double c = a - b;
     return c;
 }
